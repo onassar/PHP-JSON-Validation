90291c55a23c372e686bd5284bbd6c450f77c960
Oliver Nassar <onassar@gmail.com>
2011-12-25 17:35:23 -0500

    Example mods

---
+++ example/process.php
@@ -0,0 +1,39 @@
+<?php
+
+    /**
+     * PHP-JSON-Validation example
+     *
+     * In the spirit of a typical blog post comment (requiring a visitors name,
+     * email address, website url, and comment), the following eample
+     * demonstrates how to create a Schema instance based off a json-file
+     * schema, create a SchemaValidator object based off that Schema object and
+     * sample/dummy data, and validate them against one-another.
+     *
+     * @author Oliver Nassar <onassar@gmail.com>
+     */
+
+    // dummy data
+    $_POST = array(
+        'name' => 'Oliver Nassar',
+        'email' => 'onassar@gmail.com',
+        'website' => 'not a valid url',
+        'comment' => 'Hello World!'
+    );
+
+    // instantiation
+    require_once '../Schema.class.php';
+    require_once '../SchemaValidator.class.php';
+    $schema = (new Schema('comment.json'));
+    $validator = (new SchemaValidator($schema, $_POST));
+
+    // validation
+    if ($validator->valid()) {
+        echo 'Valid';
+    } else {
+        echo 'Invalid<br />Offending rule(s):' .
+             '<pre>';
+        $offending = $validator->getErrors();
+        print_r($offending);
+        echo '</pre>';
+    }
+    exit(0);
