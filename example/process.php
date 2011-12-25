e2782a8c46e3db75aef5cee0093153ec7476c47c
Oliver Nassar <onassar@gmail.com>
2011-12-25 17:39:18 -0500

    Documentation fix

--- example/process.php
+++ example/process.php
@@ -4,7 +4,7 @@
      * PHP-JSON-Validation example
      *
      * In the spirit of a typical blog post comment (requesting a visitors name,
-     * email address, website url, and comment), the following eample
+     * email address, website url, and comment), the following example
      * demonstrates how to create a Schema instance based off a json-file
      * schema, create a SchemaValidator object based off that Schema object and
      * sample/dummy data, and validate them against one-another.
