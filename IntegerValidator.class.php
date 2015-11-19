cb0a2ed1d68bf0d515b8675b67dd891026056e3b
Oliver Nassar <onassar@gmail.com>
2015-11-19 13:04:15 -0500

    Int ups

--- IntegerValidator.class.php
+++ IntegerValidator.class.php
@@ -14,11 +14,53 @@
          * @access public
          * @static
          * @param  integer $int
-         * @param  integer $minimum
+         * @param  integer $min
          * @return boolean
          */
-        public static function greaterThan($int, $minimum)
+        public static function greaterThan($int, $min)
         {
-            return (int) $int > (int) $minimum;
+            return (int) $int > (int) $min;
+        }
+
+        /**
+         * greaterThanOrEqualTo
+         * 
+         * @access public
+         * @static
+         * @param  integer $int
+         * @param  integer $min
+         * @return boolean
+         */
+        public static function greaterThanOrEqualTo($int, $min)
+        {
+            return (int) $int >= (int) $min;
+        }
+
+        /**
+         * lessThan
+         * 
+         * @access public
+         * @static
+         * @param  integer $int
+         * @param  integer $max
+         * @return boolean
+         */
+        public static function lessThan($int, $max)
+        {
+            return (int) $int < (int) $max;
+        }
+
+        /**
+         * lessThanOrEqualTo
+         * 
+         * @access public
+         * @static
+         * @param  integer $int
+         * @param  integer $max
+         * @return boolean
+         */
+        public static function lessThanOrEqualTo($int, $max)
+        {
+            return (int) $int <= (int) $max;
         }
     }
