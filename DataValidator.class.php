183bbbb00e34656f7826aee7bb46a8eca6d8f063
Oliver Nassar <oliver.nassar@namecheap.com>
2022-09-05 14:44:26 -0400

    More vlaidation methods

--- DataValidator.class.php
+++ DataValidator.class.php
@@ -75,4 +75,47 @@
             $valid = empty($_POST) === false;
             return $valid;
         }
+
+        /**
+         * validHTTPReferrer
+         * 
+         * @access  public
+         * @static
+         * @param   string $host
+         * @return  bool
+         */
+        public static function validHTTPReferrer(string $host): bool
+        {
+            $referrer = $_SERVER['HTTP_REFERER'] ?? null;
+            if ($referrer === null) {
+                return false;
+            }
+            $referrer = trim($referrer);
+            if ($referrer === '') {
+                return false;
+            }
+            if (str_contains($referrer, $host) === false) {
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         * validRequestMethod
+         * 
+         * @access  public
+         * @static
+         * @param   string $requestMethod
+         * @return  bool
+         */
+        public static function validRequestMethod(string $requestMethod): bool
+        {
+            $serverRequestMethod = $_SERVER['REQUEST_METHOD'];
+            $serverRequestMethod = strtolower($serverRequestMethod);
+            $requestMethod = strtolower($requestMethod);
+            if ($serverRequestMethod === $requestMethod) {
+                return true;
+            }
+            return false;
+        }
     }
