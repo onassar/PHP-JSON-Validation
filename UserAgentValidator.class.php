98cd84a90b8390d3de754bc3d660880fd2ebc2d1
Oliver Nassar <oliver.nassar@namecheap.com>
2022-09-10 18:05:46 -0400

    New validation

---
+++ UserAgentValidator.class.php
@@ -0,0 +1,27 @@
+<?php
+
+    /**
+     * UserAgentValidator
+     * 
+     * @abstract
+     * @link    https://github.com/onassar/PHP-JSON-Validation
+     * @author  Oliver Nassar <onassar@gmail.com>
+     */
+    abstract class UserAgentValidator
+    {
+        /**
+         * notEmpty
+         * 
+         * @access  public
+         * @static
+         * @return  bool
+         */
+        public static function notEmpty(): bool
+        {
+            $httpUserAgent = $_SERVER['HTTP_USER_AGENT'] ?? null;
+            if ($httpUserAgent === null) {
+                return false;
+            }
+            return true;
+        }
+    }
